24df57d9598f624eb0e6a94cc66cbb3112691ebf
Austin Hinderer <zeshon@gmail.com>
2017-12-28 19:40:21 -0800

    Corrected CircleCI config

--- .circleci/deploy-ghpages.sh
+++ .circleci/deploy-ghpages.sh
@@ -9,8 +9,7 @@ set -e
 
 # show where we are on the machine
 pwd
-# remote=$(git config remote.origin.url)
-remote="git@github.com:CityOfZion/cityofzion.github.io.git"
+remote="https://github.com/CityOfZion/cityofzion.github.io.git"
 
 # make a directory to put the gp-pages branch
 mkdir gh-pages-branch
